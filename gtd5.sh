385c37058d1d7f5164231b4de3aa2752d8a0f1a8
Eric San Gabriel <ERIC.SANGABRIEL@TELUS.COM>
2025-08-07 15:13:31 -0600

    Add shellscript

---
+++ gtd5.sh
@@ -0,0 +1,5 @@
+# source /home/t065548/VirtEnv/GTD5/bin/activate
+source /home/x249581/.venv/bin/activate
+cd /home/x249581/GTD5
+python main.py
+mv output* /devops/projects/Customer_Count/Voice/GTD5
