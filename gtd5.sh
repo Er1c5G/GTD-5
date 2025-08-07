00dbd9a2a63807a7533bd05987d32a0483f7128b
Eric San Gabriel <ERIC.SANGABRIEL@TELUS.COM>
2025-08-07 16:17:24 -0600

    Move dir's content only

--- gtd5.sh
+++ gtd5.sh
@@ -2,4 +2,4 @@
 source /home/net/x249581/GTD-5/.venv/bin/activate
 cd /home/net/x249581/GTD-5
 python main.py
-mv output* /devops/projects/Customer_Count/Voice/GTD5
+mv output/* /devops/projects/Customer_Count/Voice/GTD5
