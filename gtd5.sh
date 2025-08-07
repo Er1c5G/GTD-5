81f2abebd5865833b98e8579c837cf6ae48d8eaa
Eric San Gabriel <ERIC.SANGABRIEL@TELUS.COM>
2025-08-07 15:24:16 -0600

    Update gtd5.sh

--- gtd5.sh
+++ gtd5.sh
@@ -1,5 +1,5 @@
 # source /home/t065548/VirtEnv/GTD5/bin/activate
-source /home/x249581/.venv/bin/activate
-cd /home/x249581/GTD5
+source /home/net/x249581/GTD-5/.venv/bin/activate
+cd /home/net/x249581/GTD-5
 python main.py
 mv output* /devops/projects/Customer_Count/Voice/GTD5
